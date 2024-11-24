a9ceeb7a9e97b7bd42d0e20fcce7eb9fc32987e0
ravi9409 <raviranjan9409@gmail.com>
2024-11-25 02:07:08 +0530

    all changes

--- index.js
+++ index.js
@@ -24,14 +24,131 @@ let db;
   }
 })();
 
-async function fetchAllMovies() {
-  let query = 'SELECT * FROM movies';
+async function fetchAllRestaurants() {
+  let query = 'SELECT * FROM restaurants';
   let response = await db.all(query, []);
   return { movies: response };
 }
 
-app.get('/movies', async (req, res) => {
-  let results = await fetchAllMovies();
+async function fetchRestaurantsById(id) {
+  let query = 'SELECT * FROM restaurants WHERE id=?';
+  let response = await db.all(query, [id]);
+  return { movies: response };
+}
+
+async function fetchRestaurantsByCuisine(cuisine) {
+  let query = 'SELECT * FROM restaurants WHERE cuisine=?';
+  let response = await db.all(query, [cuisine]);
+  return { movies: response };
+}
+
+async function fetchRestaurantsByQuerys(isVeg,hasOutdoorSeating,isLuxury) {
+  if(isVeg && hasOutdoorSeating && isLuxury){
+    let query = "SELECT * FROM restaurants WHERE isVeg='true' AND hasOutdoorSeating='true' AND isLuxury='true'";
+    let response = await db.all(query, []);
+    return { movies: response };
+  }else if(isVeg && hasOutdoorSeating){
+    let query = "SELECT * FROM restaurants WHERE isVeg='true' AND hasOutdoorSeating='true'";
+    let response = await db.all(query, []);
+    return { movies: response };
+  }
+  else if(isVeg){
+    let query = "SELECT * FROM restaurants WHERE isVeg='true'";
+    let response = await db.all(query, []);
+    return { movies: response };
+  }else if(hasOutdoorSeating){
+    let query = "SELECT * FROM restaurants WHERE hasOutdoorSeating='true'";
+    let response = await db.all(query, []);
+    return { movies: response };
+  }else if(isLuxury){
+    let query = "SELECT * FROM restaurants WHERE isLuxury='true'";
+    let response = await db.all(query, []);
+    return { movies: response };
+  }
+  
+}
+
+async function fetchRestaurantsByRating() {
+  let query = 'SELECT * FROM restaurants ORDER BY rating DESC';
+  let response = await db.all(query, []);
+  return { movies: response };
+}
+
+async function fetchDishes() {
+  let query = 'SELECT * FROM dishes';
+  let response = await db.all(query, []);
+  return { movies: response };
+}
+
+async function fetchDishesById(id) {
+  let query = 'SELECT * FROM dishes WHERE id=?';
+  let response = await db.all(query, [id]);
+  return { movies: response };
+}
+
+async function fetchDishesByFilter(isVeg) {
+  if(isVeg){
+  let query = "SELECT * FROM dishes WHERE isVeg='true'";
+  let response = await db.all(query, []);
+  return { movies: response };
+  }
+}
+
+async function fetchDishesBySort() {
+  let query = 'SELECT * FROM dishes ORDER BY price ASC';
+  let response = await db.all(query, []);
+  return { movies: response };
+}
+
+app.get('/restaurants', async (req, res) => {
+  let results = await fetchAllRestaurants();
+  res.status(200).json(results);
+});
+
+app.get('/restaurants/details/:id', async (req, res) => {
+  let id=req.params.id;
+  let results = await fetchRestaurantsById(id);
+  res.status(200).json(results);
+});
+
+app.get('/restaurants/cuisine/:cuisine', async (req, res) => {
+  let cuisine=req.params.cuisine;
+  let results = await fetchRestaurantsByCuisine(cuisine);
+  res.status(200).json(results);
+});
+
+app.get('/restaurants/filter', async (req, res) => {
+  let isVeg = req.query.isVeg === 'true';
+  let hasOutdoorSeating = req.query.hasOutdoorSeating === 'true';
+  let isLuxury = req.query.isLuxury === 'true';
+  let results = await fetchRestaurantsByQuerys(isVeg,hasOutdoorSeating,isLuxury);
+  res.status(200).json(results);
+});
+
+app.get('/restaurants/sort-by-rating', async (req, res) => {
+  let results = await fetchRestaurantsByRating();
+  res.status(200).json(results);
+});
+
+app.get('/dishes', async (req, res) => {
+  let results = await fetchDishes();
+  res.status(200).json(results);
+});
+
+app.get('/dishes/details/:id', async (req, res) => {
+  let id = req.params.id;
+  let results = await fetchDishesById(id);
+  res.status(200).json(results);
+});
+
+app.get('/dishes/filter/', async (req, res) => {
+  let isVeg = req.query.isVeg==='true';
+  let results = await fetchDishesByFilter(isVeg);
+  res.status(200).json(results);
+});
+
+app.get('/dishes/sort-by-price/', async (req, res) => {
+  let results = await fetchDishesBySort();
   res.status(200).json(results);
 });
 
--- initDB.js
+++ initDB.js
@@ -1,246 +1,140 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
 // Connect to SQLite database
-const db = new sqlite3.Database('./BD4-1/database.sqlite', (err) => {
+const db = new sqlite3.Database("./BD4-1/database.sqlite", (err) => {
   if (err) {
-    console.error('Error opening database:', err.message);
+    console.error("Error opening database:", err.message);
   } else {
-    console.log('Connected to the SQLite database.');
+    console.log("Connected to the SQLite database.");
   }
 });
 
-// Create a movies table with an additional actor and box_office_collection column
 db.serialize(() => {
   db.run(
-    `CREATE TABLE IF NOT EXISTS movies (
+    `CREATE TABLE IF NOT EXISTS restaurants (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT,
-      director TEXT,
-      genre TEXT,
-      release_year INTEGER,
+      name TEXT,
+      cuisine TEXT,
+      isVeg TEXT,
       rating REAL,
-      actor TEXT,
-      box_office_collection REAL
+      priceForTwo INTEGER,
+      location TEXT,
+      hasOutdoorSeating TEXT,
+      isLuxury TEXT
     )`,
     (err) => {
       if (err) {
-        console.error('Error creating table:', err.message);
+        console.error("Error creating table:", err.message);
       } else {
-        console.log('Movies table created or already exists.');
+        console.log("Restaurants table created or already exists.");
       }
-    }
+    },
+  );
+  db.run(
+    `CREATE TABLE IF NOT EXISTS dishes (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT,
+      isVeg TEXT,
+      rating REAL,
+      price INTEGER
+    )`,
+    (err) => {
+      if (err) {
+        console.error("Error creating table:", err.message);
+      } else {
+        console.log("Dishes table created or already exists.");
+      }
+    },
   );
 
-  // Insert random movie data
   const stmt = db.prepare(
-    'INSERT INTO movies (title, director, genre, release_year, rating, actor, box_office_collection) VALUES (?, ?, ?, ?, ?, ?, ?)'
+    "INSERT INTO restaurants (name, cuisine, isVeg, rating, priceForTwo, location, hasOutdoorSeating, isLuxury) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+  );
+  const stmt2 = db.prepare(
+    "INSERT INTO dishes (name, isVeg, rating, price) VALUES (?, ?, ?, ?)",
   );
 
-  const movies = [
-    // Unique Bollywood Movies
+  const restaurants = [
     {
-      id: 1,
-      title: 'Dangal',
-      director: 'Nitesh Tiwari',
-      genre: 'Biography',
-      release_year: 2016,
-      rating: 4.8,
-      actor: 'Aamir Khan',
-      box_office_collection: 220,
-    },
-    {
-      id: 2,
-      title: 'Baahubali 2: The Conclusion',
-      director: 'S.S. Rajamouli',
-      genre: 'Action',
-      release_year: 2017,
-      rating: 4.7,
-      actor: 'Prabhas',
-      box_office_collection: 181,
-    },
-    {
-      id: 3,
-      title: 'PK',
-      director: 'Rajkumar Hirani',
-      genre: 'Comedy',
-      release_year: 2014,
-      rating: 4.6,
-      actor: 'Aamir Khan',
-      box_office_collection: 140,
-    },
-    {
-      id: 4,
-      title: 'Bajrangi Bhaijaan',
-      director: 'Kabir Khan',
-      genre: 'Drama',
-      release_year: 2015,
+      name: "Spice Kitchen",
+      cuisine: "Indian",
+      isVeg: "true",
       rating: 4.5,
-      actor: 'Salman Khan',
-      box_office_collection: 130,
+      priceForTwo: 1500,
+      location: "MG Road",
+      hasOutdoorSeating: "true",
+      isLuxury: "false",
     },
     {
-      id: 5,
-      title: 'Sultan',
-      director: 'Ali Abbas Zafar',
-      genre: 'Drama',
-      release_year: 2016,
-      rating: 4.3,
-      actor: 'Salman Khan',
-      box_office_collection: 120,
-    },
-    {
-      id: 6,
-      title: 'Sanju',
-      director: 'Rajkumar Hirani',
-      genre: 'Biography',
-      release_year: 2018,
-      rating: 4.4,
-      actor: 'Ranbir Kapoor',
-      box_office_collection: 120,
-    },
-    {
-      id: 7,
-      title: 'Padmaavat',
-      director: 'Sanjay Leela Bhansali',
-      genre: 'Drama',
-      release_year: 2018,
+      name: "Olive Bistro",
+      cuisine: "Italian",
+      isVeg: "false",
       rating: 4.2,
-      actor: 'Ranveer Singh',
-      box_office_collection: 112,
+      priceForTwo: 2000,
+      location: "Jubilee Hills",
+      hasOutdoorSeating: "false",
+      isLuxury: "true",
     },
     {
-      id: 8,
-      title: '3 Idiots',
-      director: 'Rajkumar Hirani',
-      genre: 'Comedy',
-      release_year: 2009,
-      rating: 4.9,
-      actor: 'Aamir Khan',
-      box_office_collection: 202,
-    },
-    {
-      id: 9,
-      title: 'Chennai Express',
-      director: 'Rohit Shetty',
-      genre: 'Comedy',
-      release_year: 2013,
+      name: "Green Leaf",
+      cuisine: "Chinese",
+      isVeg: "true",
       rating: 4.0,
-      actor: 'Shah Rukh Khan',
-      box_office_collection: 100,
+      priceForTwo: 1000,
+      location: "Banjara Hills",
+      hasOutdoorSeating: "false",
+      isLuxury: "false",
     },
+  ];
+  const dishes = [
     {
-      id: 10,
-      title: 'War',
-      director: 'Siddharth Anand',
-      genre: 'Action',
-      release_year: 2019,
-      rating: 4.3,
-      actor: 'Hrithik Roshan',
-      box_office_collection: 100,
-    },
-    {
-      id: 11,
-      title: 'Kabir Singh',
-      director: 'Sandeep Reddy Vanga',
-      genre: 'Romance',
-      release_year: 2019,
-      rating: 4.2,
-      actor: 'Shahid Kapoor',
-      box_office_collection: 80,
-    },
-    {
-      id: 12,
-      title: 'Gully Boy',
-      director: 'Zoya Akhtar',
-      genre: 'Drama',
-      release_year: 2019,
-      rating: 4.4,
-      actor: 'Ranveer Singh',
-      box_office_collection: 75,
-    },
-    {
-      id: 13,
-      title: 'Andhadhun',
-      director: 'Sriram Raghavan',
-      genre: 'Thriller',
-      release_year: 2018,
+      name: "Paneer Butter Masala",
+      price: 300,
       rating: 4.5,
-      actor: 'Ayushmann Khurrana',
-      box_office_collection: 60,
+      isVeg: "true",
     },
     {
-      id: 14,
-      title: 'Tumbbad',
-      director: 'Rahi Anil Barve',
-      genre: 'Horror',
-      release_year: 2018,
-      rating: 4.3,
-      actor: 'Sohum Shah',
-      box_office_collection: 50,
-    },
-    {
-      id: 15,
-      title: 'Stree',
-      director: 'Amar Kaushik',
-      genre: 'Comedy',
-      release_year: 2018,
-      rating: 4.0,
-      actor: 'Rajkummar Rao',
-      box_office_collection: 60,
-    },
-    {
-      id: 16,
-      title: 'Badhaai Ho',
-      director: 'Amit Sharma',
-      genre: 'Comedy',
-      release_year: 2018,
-      rating: 4.2,
-      actor: 'Ayushmann Khurrana',
-      box_office_collection: 45,
-    },
-    {
-      id: 17,
-      title: 'Article 15',
-      director: 'Anubhav Sinha',
-      genre: 'Drama',
-      release_year: 2019,
-      rating: 4.6,
-      actor: 'Ayushmann Khurrana',
-      box_office_collection: 35,
-    },
-    {
-      id: 18,
-      title: 'URI: The Surgical Strike',
-      director: 'Aditya Dhar',
-      genre: 'Action',
-      release_year: 2019,
+      name: "Chicken Alfredo Pasta",
+      price: 500,
       rating: 4.7,
-      actor: 'Vicky Kaushal',
-      box_office_collection: 70,
+      isVeg: "false",
+    },
+    {
+      name: "Veg Hakka Noodles",
+      price: 250,
+      rating: 4.3,
+      isVeg: "true",
     },
   ];
 
-  for (let movie of movies) {
+  for (let restaurant of restaurants) {
     stmt.run(
-      movie.title,
-      movie.director,
-      movie.genre,
-      movie.release_year,
-      movie.rating,
-      movie.actor,
-      movie.box_office_collection
+      restaurant.name,
+      restaurant.cuisine,
+      restaurant.isVeg,
+      restaurant.rating,
+      restaurant.priceForTwo,
+      restaurant.location,
+      restaurant.hasOutdoorSeating,
+      restaurant.isLuxury,
     );
   }
-  stmt.finalize();
+  for (let dish of dishes) {
+    stmt2.run(dish.name, dish.isVeg, dish.rating, dish.price);
+  }
 
-  console.log('Inserted 18 Bollywood movies into the database.');
+  stmt.finalize();
+  stmt2.finalize();
+
+  console.log("Inserted 3 restaurants into the database.");
+  console.log("Inserted 3 dishes into the database.");
 
   // Close the database connection
   db.close((err) => {
     if (err) {
-      console.error('Error closing database:', err.message);
+      console.error("Error closing database:", err.message);
     } else {
-      console.log('Database connection closed.');
+      console.log("Database connection closed.");
     }
   });
 });
